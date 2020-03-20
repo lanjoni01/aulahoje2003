6e1806d05276fede09acbe8065f5175a65de227f
lanjoni01 <lanjoni01@users.noreply.github.com>
2020-03-20 20:31:11 -0300

    resolução exercicios arthur 20/03

--- src/aula20200320/testesGerais/GerenciadorDeComissao.java
+++ src/aula20200320/testesGerais/GerenciadorDeComissao.java
@@ -1,9 +1,34 @@
 package aula20200320.testesGerais;
 
 public class GerenciadorDeComissao {
-	
+
 	public static double calcularComissaoPadrao(double valorDaVenda, double percentualDeComissao) {
-		return valorDaVenda * (percentualDeComissao/100.00);
+		return valorDaVenda * (percentualDeComissao / 100.00);
+	}
+
+	public static double calcularComissaoComValorMinimo(double valorMinimo, double valorDaVenda,
+			double percentualDeComissao) {
+		double conseguiuValorMinimo = valorDaVenda * percentualDeComissao / 100;
+
+		if (conseguiuValorMinimo >= valorMinimo) {
+			return conseguiuValorMinimo;
+		}
+
+		return valorMinimo;
+	}
+
+	public static double calcularComissaoComFaixa(double valorMinimo, double valorMaximo, double valorDaVenda,
+			double percentualDeComissao) {
+		double valorTotalComissao = valorDaVenda * percentualDeComissao / 100;
+
+		if (valorTotalComissao >= valorMinimo && valorTotalComissao <= valorMaximo) {
+			return valorTotalComissao;
+		}
+
+		if (valorTotalComissao <= valorMinimo) {
+			return valorMinimo;
+		}
+		return valorMaximo;
 	}
 
 }
--- src/aula20200320/testesGerais/Testes.java
+++ src/aula20200320/testesGerais/Testes.java
@@ -10,88 +10,80 @@ class Testes {
 	void testarSomaDeDoisInteiros() {
 		int valor1 = 100;
 		int valor2 = 300;
-		int total = valor1+valor2;
-		
+		int total = valor1 + valor2;
+
 		assertEquals(400, total);
 	}
-	
+
 	@Test
 	void testarSubtracaoDeDoisInteiros() {
 		int valor1 = 100;
 		int valor2 = 300;
-		int total = valor1-valor2;
-		
+		int total = valor1 - valor2;
+
 		assertEquals(-200, total);
 	}
-	
-	@Test 
+
+	@Test
 	void testarComissaoDoVendedor() {
 		double valorDaVenda = 5000.00;
 		double percentualDeComissao = 10.00;
 		double valorDaComissao = GerenciadorDeComissao.calcularComissaoPadrao(valorDaVenda, percentualDeComissao);
-		
+
 		assertEquals(500.00, valorDaComissao);
-		
+
 		valorDaVenda = 1000.00;
 		percentualDeComissao = 3.00;
 		valorDaComissao = GerenciadorDeComissao.calcularComissaoPadrao(valorDaVenda, percentualDeComissao);
-		
+
 		assertEquals(30.00, valorDaComissao);
 	}
-	
-	@Test 
+
+	@Test
 	void testarComissaoDoVendedorComValorMinimo() {
 		final double valorMinimo = 100.00;
-		
+
 		double valorDaVenda = 5000.00;
 		double percentualDeComissao = 10.00;
-		double valorDaComissao = GerenciadorDeComissao.calcularComissaoComValorMinimo(valorMinimo, valorDaVenda, percentualDeComissao);
-		
+		double valorDaComissao = GerenciadorDeComissao.calcularComissaoComValorMinimo(valorMinimo, valorDaVenda,
+				percentualDeComissao);
+
 		assertEquals(500.00, valorDaComissao);
-		
+
 		valorDaVenda = 1000.00;
 		percentualDeComissao = 3.00;
-		valorDaComissao = GerenciadorDeComissao.calcularComissaoComValorMinimo(valorMinimo, valorDaVenda, percentualDeComissao);
-		
+		valorDaComissao = GerenciadorDeComissao.calcularComissaoComValorMinimo(valorMinimo, valorDaVenda,
+				percentualDeComissao);
+
 		assertEquals(100.00, valorDaComissao);
 	}
-	
-	@Test 
+
+	@Test
 	void testarComissaoDoVendedorComFaixaDeValores() {
 		final double valorMinimo = 80.00;
 		final double valorMaximo = 350.00;
-		
+
 		double valorDaVenda = 5000.00;
 		double percentualDeComissao = 10.00;
-		double valorDaComissao = GerenciadorDeComissao.calcularComissaoComFaixa(
-				valorMinimo, valorMaximo, valorDaVenda, percentualDeComissao);
-		
+		double valorDaComissao = GerenciadorDeComissao.calcularComissaoComFaixa(valorMinimo, valorMaximo, valorDaVenda,
+				percentualDeComissao);
+
 		assertEquals(valorMaximo, valorDaComissao);
-		
+
 		valorDaVenda = 1000.00;
 		percentualDeComissao = 3.00;
-		valorDaComissao = GerenciadorDeComissao.calcularComissaoComFaixa(
-				valorMinimo, valorMaximo, valorDaVenda, percentualDeComissao);
-		
+		valorDaComissao = GerenciadorDeComissao.calcularComissaoComFaixa(valorMinimo, valorMaximo, valorDaVenda,
+				percentualDeComissao);
+
 		assertEquals(valorMinimo, valorDaComissao);
-		
+
 		valorDaVenda = 2000.00;
 		percentualDeComissao = 6.00;
-		valorDaComissao = GerenciadorDeComissao.calcularComissaoComFaixa(
-				valorMinimo, valorMaximo, valorDaVenda, percentualDeComissao);
-		
+		valorDaComissao = GerenciadorDeComissao.calcularComissaoComFaixa(valorMinimo, valorMaximo, valorDaVenda,
+				percentualDeComissao);
+
 		assertEquals(120.00, valorDaComissao);
-		
+
 	}
-	
 
 }
-
-
-
-
-
-
-
-
-
